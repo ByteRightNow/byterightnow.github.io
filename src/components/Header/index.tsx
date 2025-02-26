ce5424617fa597a69442c37414245a4c7f0e31aa
Sohanuzzaman Soad <mdsoad@gmail.com>
2025-02-26 18:49:54 +0600

    refactor button styles, update header logo size, modify footer contact details and address, and adjust footer colors for consistency

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -59,7 +59,7 @@ const Header = ({ t }: { t: TFunction }) => {
       <Container>
         <Row justify="space-between">
           <LogoContainer to="/" aria-label="homepage">
-            <SvgIcon src="logo.svg" width="80px" height="80px" />
+            <SvgIcon src="logo.svg" width="180px" height="80px" />
           </LogoContainer>
           <NotHidden>
             <MenuItem />
